abcbf96a62788e35c70eccd9648923cff57e5ba5
Allen <tuk04440@temple.edu>
2024-04-03 15:18:25 -0400

    Added intent to send text in EditText and app shows toast if no text is entered.

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,8 +1,10 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,12 +12,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // This view contains the text to share
         val editText = findViewById<EditText>(R.id.editTextText)
 
-        // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+            val textToShare = editText.text.toString()
 
+            if (textToShare.isEmpty()) {
+                Toast.makeText(this, "Please enter some text to share.", Toast.LENGTH_SHORT).show()
+            } else {
+                val shareIntent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, textToShare)
+                }
+                startActivity(Intent.createChooser(shareIntent, "Share via"))
+            }
         }
     }
 }
